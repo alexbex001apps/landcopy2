7995574ac54a02e4233226ab41faa080b80d1900
alexbex001apps <alexbex001@gmail.com>
2026-05-28 11:03:26 -0500

    primer commit landcopy2

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black flex flex-col items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">LandCopy 2.0</h1>
+      <p className="text-orange-500 text-xl mt-4">Tu plataforma de marketing visual</p>
+    </main>
+  );
+}
